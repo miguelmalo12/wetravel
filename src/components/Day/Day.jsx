32811f955235e55322dcda63429d026820dca1c1
miguelmalo12 <miguelmalo12@gmail.com>
2023-06-06 23:47:58 -0700

    Added drag functionality

--- src/components/Day/Day.jsx
+++ src/components/Day/Day.jsx
@@ -1,24 +1,43 @@
-import './Day.scss';
-import dayIcon from '../../assets/icons/day-icon.svg'
+import "./Day.scss";
+import { useState } from "react";
+import dayIcon from "../../assets/icons/day-icon.svg";
 
-function Day( { dayNumber, date } ) {
-  return (
-    <div className='day'>
-        <div className='day--card'>
-            <div className='day--card__icon'>
-                <img src={dayIcon} alt="" />
-            </div>
-            <div className='day--card__text'>
-                <h5>Day {dayNumber}</h5>
-                <p>{date}</p>
-            </div>
+function Day({ dayNumber, date }) {
+    const [events, setEvents] = useState([]);
+  
+    return (
+    <div className="day">
+      <div className="day--card">
+        <div className="day--card__icon">
+          <img src={dayIcon} alt="" />
         </div>
-        <div className='day--area'>
-            <p>Drag Here</p>
+        <div className="day--card__text">
+          <h5>Day {dayNumber}</h5>
+          <p>{date}</p>
         </div>
-        <div className='day--line'></div>
+      </div>
+      {events.map((event, index) => (
+          <div key={index}>
+            {event.title} <input type="text" />
+          </div>
+        ))}
+      <div
+        className="day--area"
+        onDragOver={(e) => {
+          e.preventDefault(); // This is necessary to allow a drop
+        }}
+        onDrop={(e) => {
+          e.preventDefault();
+          const eventTitle = e.dataTransfer.getData("text/plain");
+          setEvents([...events, { title: eventTitle }]);
+        }}
+      >
+        <p>Drag Here</p>
+        
+      </div>
+      <div className="day--line"></div>
     </div>
-  )
+  );
 }
 
-export default Day
+export default Day;
--- src/components/TravelPlanner/TravelPlanner.jsx
+++ src/components/TravelPlanner/TravelPlanner.jsx
@@ -1,13 +1,13 @@
-import './TravelPlanner.scss';
-import Day from '../Day/Day';
-import { addDays, format } from 'date-fns';
+import "./TravelPlanner.scss";
+import Day from "../Day/Day";
+import { addDays, format } from "date-fns";
 
-import transportationIcon from '../../assets/icons/TransportationIcon.png';
-import accommodationIcon from '../../assets/icons/AccommodationIcon.png';
-import activityIcon from '../../assets/icons/ActivityIcon.png';
-import restaurantIcon from '../../assets/icons/RestaurantIcon.png';
+import transportationIcon from "../../assets/icons/TransportationIcon.png";
+import accommodationIcon from "../../assets/icons/AccommodationIcon.png";
+import activityIcon from "../../assets/icons/ActivityIcon.png";
+import restaurantIcon from "../../assets/icons/RestaurantIcon.png";
 
-function TravelPlanner( { location, dayCount, startDate } ) {
+function TravelPlanner({ location, dayCount, startDate }) {
   return (
     <div className="planner">
       <div className="planner--title">
@@ -15,21 +15,33 @@ function TravelPlanner( { location, dayCount, startDate } ) {
       </div>
       <div className="planner--plan">
         <div className="planner--plan__days">
-        {Array.from({ length: dayCount }, (_, i) => {
-              const date = addDays(new Date(startDate), i);
-              const formattedDate = format(date, 'E, dd MMM'); // format the date as desired
-              return <Day key={i} dayNumber={i + 1} date={formattedDate} />
-            })}
+          {Array.from({ length: dayCount }, (_, i) => {
+            const date = addDays(new Date(startDate), i);
+            const formattedDate = format(date, "E, dd MMM"); // format the date as desired
+            return <Day key={i} dayNumber={i + 1} date={formattedDate} />;
+          })}
         </div>
         <div className="planner--plan__events">
           <div className="planner--plan__events--title">
             <h3>Events</h3>
           </div>
-          <div className="planner--plan__events--item">
+          <div
+            className="planner--plan__events--item"
+            draggable="true"
+            onDragStart={(e) => {
+              e.dataTransfer.setData("text/plain", "Add Transportation");
+            }}
+          >
             <img src={transportationIcon} alt="" />
             <h5>Add Transportation</h5>
           </div>
-          <div className="planner--plan__events--item">
+          <div
+            className="planner--plan__events--item"
+            draggable="true"
+            onDragStart={(e) => {
+              e.dataTransfer.setData("text/plain", "Add Accommodation");
+            }}
+          >
             <img src={accommodationIcon} alt="" />
             <h5>Add Accommodation</h5>
           </div>
@@ -47,4 +59,4 @@ function TravelPlanner( { location, dayCount, startDate } ) {
   );
 }
 
-export default TravelPlanner
+export default TravelPlanner;
