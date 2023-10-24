f1070f588c26bfee2833647f3b801e322aac8275
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2023-10-23 19:00:55 -0700

    Navbar created for desktop screen size

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,70 +1,40 @@
-import "./Header.scss";
-import { Link, useLocation } from "react-router-dom";
-import logo from "../../assets/wetravel-logo-red.png";
-import burgerMenu from '../../assets/bars-solid.svg';
+import './Header.scss';
+import { Link } from 'react-router-dom';
+import WeTravelLogo from '../../assets/wetravel-logo-red.png';
 
-function Header() {
-  const location = useLocation();
+
+const Header = () => {
 
   return (
     <header className="header">
+
+      <Link className="header__logo-container">
+        <img src={WeTravelLogo} alt="WeTravel Logo" className="header__logo" />
+      </Link>
+
       <nav className="navbar">
-        <Link to="/" className="navbar__logo"> <img src={logo} alt="wetravel logo" /> </Link>
-        <div className="navbar__menu-mobile">
-          <img src={burgerMenu} alt="bars menu" />
-        </div>
-        <div className="navbar__menu">
-          <ul className="navbar__list">
-            <li
-              className={`navbar__item ${
-                location.pathname === "/" ? "navbar__item--active" : ""
-              }`}
-            >
-              <Link to="/" className="navbar__link">
-                Home
-              </Link>
-            </li>
-            <li
-              className={`navbar__item ${
-                location.pathname === "/recommend" ? "navbar__item--active" : ""
-              }`}
-            >
-              <Link to="/recommend" className="navbar__link">
-                Recommend
-              </Link>
-            </li>
-            <li
-              className={`navbar__item ${
-                location.pathname === "/plan" ? "navbar__item--active" : ""
-              }`}
-            >
-              <Link to="/plan" className="navbar__link">
-                Plan
-              </Link>
-            </li>
-            <li
-              className={`navbar__item ${
-                location.pathname === "/login" ? "navbar__item--active" : ""
-              }`}
-            >
-              <Link to="/login" className="navbar__link primary-button">
-                Login
-              </Link>
-            </li>
-            <li
-              className={`navbar__item ${
-                location.pathname === "/signup" ? "navbar__item--active" : ""
-              }`}
-            >
-              <Link to="/signup" className="navbar__link--light">
-                Signup
-              </Link>
-            </li>
-          </ul>
-        </div>
+        <ul className="navbar__list">
+          <li className="navbar__item">
+            <Link className="navbar__link navbar__link--active">Home</Link>
+          </li>
+          <li className="navbar__item">
+            <Link className="navbar__link">Recommend</Link>
+          </li>
+          <li className="navbar__item">
+            <Link className="navbar__link">Plan</Link>
+          </li>
+          <li className="navbar__item">
+            <Link className="navbar__link navbar__link--button-primary">Login</Link>
+          </li>
+          <li className="navbar__item">
+            <Link className="navbar__link navbar__link--button-secondary">Signup</Link>
+          </li>
+        </ul>
       </nav>
     </header>
-  );
+  )
+
+
 }
 
 export default Header;
