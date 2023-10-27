7a1fb6bfa23ec1639f6fc96dd85cbeed96a57be3
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2023-10-26 19:12:54 -0700

    Working toward responsiveness of header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,22 +12,53 @@ const Header = () => {
         <img src={WeTravelLogo} alt="WeTravel Logo" className="header__logo" />
       </Link>
 
-      <nav className="navbar">
-        <ul className="navbar__list">
-          <li className="navbar__item">
-            <Link className="navbar__link navbar__link--active">Home</Link>
+
+
+
+      <div class="navigation">
+        <input type="checkbox" class="navigation__checkbox" id="navi-toggle" />
+        <label for="navi-toggle" class="navigation__button">
+          <span class="navigation__icon">&nbsp;</span>
+        </label>
+
+        <div class="navigation__background">&nbsp;</div>
+
+        <nav class="navigation__nav">
+          <ul class="navigation__list">
+            <li class="navigation__item"><a href="#" class="navbar-desktop__link">Home</a></li>
+            <li class="navigation__item"><a href="#" class="navbar-desktop__link">Recommend</a></li>
+            <li class="navigation__item"><a href="#" class="navbar-desktop__link">Plan</a></li>
+            <li class="navigation__item"><a href="#" class="navbar-desktop__link navbar-desktop__link--button-primary">Login</a></li>
+            <li class="navigation__item"><a href="#" class="navbar-desktop__link navbar-desktop__link--button-secondary">Signup</a></li>
+          </ul>
+        </nav>
+      </div>
+
+
+
+
+
+
+
+
+
+
+      <nav className="navbar-desktop">
+        <ul className="navbar-desktop__list">
+          <li className="navbar-desktop__item">
+            <Link className="navbar-desktop__link navbar-desktop__link--active">Home</Link>
           </li>
-          <li className="navbar__item">
-            <Link className="navbar__link">Recommend</Link>
+          <li className="navbar-desktop__item">
+            <Link className="navbar-desktop__link">Recommend</Link>
           </li>
-          <li className="navbar__item">
-            <Link className="navbar__link">Plan</Link>
+          <li className="navbar-desktop__item">
+            <Link className="navbar-desktop__link">Plan</Link>
           </li>
-          <li className="navbar__item">
-            <Link className="navbar__link navbar__link--button-primary">Login</Link>
+          <li className="navbar-desktop__item">
+            <Link className="navbar-desktop__link navbar-desktop__link--button-primary">Login</Link>
           </li>
-          <li className="navbar__item">
-            <Link className="navbar__link navbar__link--button-secondary">Signup</Link>
+          <li className="navbar-desktop__item">
+            <Link className="navbar-desktop__link navbar-desktop__link--button-secondary">Signup</Link>
           </li>
         </ul>
       </nav>
