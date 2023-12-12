e716648355865f4c113eddc4c7139c91c3484366
Ahmad Rashid Akhtar <ahmadrashidakhtar@gmail.com>
2023-12-11 20:09:30 -0800

    onFocus border added

--- src/components/HeroFormInput/HeroFormInput.jsx
+++ src/components/HeroFormInput/HeroFormInput.jsx
@@ -1,13 +1,16 @@
 import './HeroFormInput.scss';
 import { ButtonHeroFormSubmit } from '../Button/Button';
+import { useFocusHandlers } from '../../formUtils';
 
 export const SingleHeroFormSelect = ({ icon, label, options, name }) => {
+    const { isFocused, handleFocus, handleBlur } = useFocusHandlers();
+
     return (
-        <div className="hero-form-input__group-container">
+        <div className={`hero-form-input__group-container ${isFocused ? 'focused' : ''}`}>
             <img src={icon} alt="" className="hero-form-input__icon" />
             <div className="hero-form-input__group">
                 <label htmlFor={name} className="hero-form-input__label">{label}</label>
-                <select type="text" name={name} className="hero-form-input__input hero-form-input__input--select" >
+                <select type="text" name={name} className={`hero-form-input__input hero-form-input__input--select`} onFocus={handleFocus} onBlur={handleBlur}>
                     {options.map((option) => {
                         return (
                             <option value={option} className="hero-form-input__input hero-form-input__input--option">{option}</option>
@@ -20,12 +23,13 @@ export const SingleHeroFormSelect = ({ icon, label, options, name }) => {
 }
 
 export const SingleHeroFormSelectSubmit = ({ icon, label, options, name }) => {
+    const { isFocused, handleFocus, handleBlur } = useFocusHandlers();
     return (
-        <div className="hero-form-input__group-container hero-form-input__group-container--submit-button">
+        <div className={`hero-form-input__group-container hero-form-input__group-container--submit-button ${isFocused ? 'focused' : ''}`}>
             <img src={icon} alt="" className="hero-form-input__icon" />
             <div className="hero-form-input__group">
                 <label htmlFor={name} className="hero-form-input__label">{label}</label>
-                <select type="text" name={name} className="hero-form-input__input hero-form-input__input--select" >
+                <select type="text" name={name} className="hero-form-input__input hero-form-input__input--select" onFocus={handleFocus} onBlur={handleBlur}>
                     {options.map((option) => {
                         return (
                             <option value={option} className="hero-form-input__input hero-form-input__input--option">{option}</option>
@@ -40,13 +44,17 @@ export const SingleHeroFormSelectSubmit = ({ icon, label, options, name }) => {
 
 
 export const DoubleHeroFormSelect = ({ icon1, icon2, label1, label2, name1, name2, options1, options2 }) => {
+    const { isFocused: isFocused1, handleFocus: handleFocus1, handleBlur: handleBlur1 } = useFocusHandlers();
+    const { isFocused: isFocused2, handleFocus: handleFocus2, handleBlur: handleBlur2 } = useFocusHandlers();
+
+
     return (
         <div className="hero-form-input__wrapper hero-form-input__wrapper--simple">
-            <div className="hero-form-input__group-container hero-form-input__group-container--double">
+            <div className={`hero-form-input__group-container hero-form-input__group-container--double ${isFocused1 ? 'focused' : ''}`}>
                 <img src={icon1} alt="" className="hero-form-input__icon" />
                 <div className="hero-form-input__group">
                     <label htmlFor={name1} className="hero-form-input__label">{label1}</label>
-                    <select name={name1} type="text" className="hero-form-input__input hero-form-input__input--select" >
+                    <select name={name1} type="text" className="hero-form-input__input hero-form-input__input--select" onFocus={handleFocus1} onBlur={handleBlur1}>
                         {options1.map((option) => {
                             return (
                                 <option value={option} className="hero-form-input__input hero-form-input__input--option">{option}</option>
@@ -55,11 +63,11 @@ export const DoubleHeroFormSelect = ({ icon1, icon2, label1, label2, name1, name
                     </select>
                 </div>
             </div>
-            <div className="hero-form-input__group-container hero-form-input__group-container--double">
+            <div className={`hero-form-input__group-container hero-form-input__group-container--double ${isFocused2 ? 'focused' : ''}`}>
                 <img src={icon2} alt="" className="hero-form-input__icon" />
                 <div className="hero-form-input__group">
                     <label htmlFor={name2} className="hero-form-input__label">{label2}</label>
-                    <select name={name2} type="text" className="hero-form-input__input hero-form-input__input--select" >
+                    <select name={name2} type="text" className="hero-form-input__input hero-form-input__input--select" onFocus={handleFocus2} onBlur={handleBlur2}>
                         {options2.map((option) => {
                             return (
                                 <option value={option} className="hero-form-input__input hero-form-input__input--option">{option}</option>
@@ -74,13 +82,17 @@ export const DoubleHeroFormSelect = ({ icon1, icon2, label1, label2, name1, name
 
 
 export const DoubleHeroFormSelectSubmit = ({ icon1, icon2, label1, label2, name1, name2, options1, options2 }) => {
+    const { isFocused: isFocused1, handleFocus: handleFocus1, handleBlur: handleBlur1 } = useFocusHandlers();
+    const { isFocused: isFocused2, handleFocus: handleFocus2, handleBlur: handleBlur2 } = useFocusHandlers();
+
+
     return (
         <div className="hero-form-input__wrapper">
-            <div className="hero-form-input__group-container hero-form-input__group-container--double">
+            <div className={`hero-form-input__group-container hero-form-input__group-container--double ${isFocused1 ? 'focused' : ''}`}>
                 <img src={icon1} alt="" className="hero-form-input__icon" />
                 <div className="hero-form-input__group">
                     <label htmlFor={name1} className="hero-form-input__label">{label1}</label>
-                    <select name={name1} type="text" className="hero-form-input__input hero-form-input__input--select" >
+                    <select name={name1} type="text" className="hero-form-input__input hero-form-input__input--select" onFocus={handleFocus1} onBlur={handleBlur1}>
                         {options1.map((option) => {
                             return (
                                 <option value={option} className="hero-form-input__input hero-form-input__input--option">{option}</option>
@@ -89,11 +101,11 @@ export const DoubleHeroFormSelectSubmit = ({ icon1, icon2, label1, label2, name1
                     </select>
                 </div>
             </div>
-            <div className="hero-form-input__group-container hero-form-input__group-container--double hero-form-input__group-container--submit  hero-form-input__group-container--submit-button">
+            <div className={`hero-form-input__group-container hero-form-input__group-container--double hero-form-input__group-container--submit  hero-form-input__group-container--submit-button ${isFocused2 ? 'focused' : ''}`}>
                 <img src={icon2} alt="" className="hero-form-input__icon" />
                 <div className="hero-form-input__group">
                     <label htmlFor={name1} className="hero-form-input__label">{label2}</label>
-                    <select name={name2} type="text" className="hero-form-input__input hero-form-input__input--select" >
+                    <select name={name2} type="text" className="hero-form-input__input hero-form-input__input--select" onFocus={handleFocus2} onBlur={handleBlur2}>
                         {options2.map((option) => {
                             return (
                                 <option value={option} className="hero-form-input__input hero-form-input__input--option">{option}</option>
